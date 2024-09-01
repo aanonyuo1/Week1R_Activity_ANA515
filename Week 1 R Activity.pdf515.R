d93f324d6311f7619c35f63572e3a2f297fa29a6
aanonyuo1 <augustine.anonyuo@gmail.com>
2024-09-01 15:47:13 -0700

    Add files via upload

---
+++ Week 1 R Activity.pdf515.R
@@ -0,0 +1,5 @@
+#Week1 Activity
+file_path <- "C:\\Users\\augus\\Downloads\\Anna Sample Markdown.Rmd"
+output_path <- "C:/Users/augus/Downloads/Anna_Sample_Output.pdf"
+render(input = file_path, output_format = "pdf_document", 
+output_file = output_path)
